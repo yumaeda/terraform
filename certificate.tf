5b5381d99121541288bca2b2dce32ac51acc2cc2
Yukitaka Maeda <yumaeda@mercari.com>
2022-10-29 16:59:51 +0900

    Remove configuration for managed certificate

--- certificate.tf
+++
@@ -1,8 +0,0 @@
-# Create HTTPS certificate
-resource "google_compute_managed_ssl_certificate" "website" {
-  provider = google
-  name     = "website-cert"
-  managed {
-    domains = [google_dns_record_set.website.name]
-  }
-}
