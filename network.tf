cbd1b877c8390106c2995068d8239734587fb1c3
Yukitaka Maeda <yumaeda@mercari.com>
2023-03-23 21:26:49 +0900

    Remove DNS Zone from GCP

--- network.tf
+++ network.tf
@@ -3,9 +3,3 @@ resource "google_compute_global_address" "website" {
   provider = google
   name     = "website-lb-ip"
 }
-
-# Get the managed DNS zone
-data "google_dns_managed_zone" "gcp_ramen_mania_zone" {
-  provider = google
-  name     = "ramen-mania-net"
-}
