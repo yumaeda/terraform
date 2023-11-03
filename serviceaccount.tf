7538d094409b23d07bf7c84febc85ea2c4c21733
Yukitaka Maeda <yumaeda@mercari.com>
2023-11-03 19:25:35 +0900

    Add storage.admin permission the github service account

--- serviceaccount.tf
+++ serviceaccount.tf
@@ -32,14 +32,26 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
     }
 }
 
-resource "google_service_account_iam_member" "github_workload_identity_user" {
+resource "google_service_account_iam_member" "github_workload_identity_nginx_gcs_proxy" {
     service_account_id = google_service_account.github.id
     role               = "roles/iam.workloadIdentityUser"
     member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/yumaeda/nginx-gcs-proxy"
 }
 
+resource "google_service_account_iam_member" "github_workload_identity_sakaba_front" {
+    service_account_id = google_service_account.github.id
+    role               = "roles/iam.workloadIdentityUser"
+    member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/yumaeda/sakaba-front"
+}
+
 resource "google_project_iam_member" "artifact_registry_admin" {
     project = var.project
     role    = "roles/artifactregistry.admin"
+    member  = "serviceAccount:${google_service_account.github.email}"
+}
+
+resource "google_project_iam_member" "storage_admin" {
+    project = var.project
+    role    = "roles/storage.admin"
     member  = "serviceAccount:${google_service_account.github.email}"
 }
