6c78f3ee12f3df859dcae89e65131a99387e63ab
Yukitaka Maeda <yumaeda@gmail.com>
2023-11-26 01:51:22 +0900

    Set default network tier to PREMIUM

--- ip.tf
+++ ip.tf
@@ -1,3 +1,7 @@
+resource "google_compute_project_default_network_tier" "default" {
+  network_tier = "PREMIUM"
+}
+
 # Reserve an external IP
 resource "google_compute_global_address" "website" {
   provider = google
@@ -7,5 +11,5 @@ resource "google_compute_global_address" "website" {
 resource "google_compute_address" "nat" {
   name   = "nat-ip"
   region = var.region
-  network_tier = "PREMIUM"
+  network_tier = google_compute_project_default_network_tier.default.network_tier
 }
