a94178d2dfc906bbe78a0bc50929db16ebad9b24
Yukitaka Maeda <yumaeda@gmail.com>
2024-05-21 19:37:35 +0900

    Change the location of the tfstate GCS bucket

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "random_id" "instance_id" {
 resource "google_storage_bucket" "default" {
   name          = "bucket-tfstate-${random_id.instance_id.hex}"
   force_destroy = false
-  location      = "US"
+  location      = var.region
   storage_class = "STANDARD"
   versioning {
     enabled = true
