3f90c6313ddffbdedaf9a10f0610782e1ee5336f
Yukitaka Maeda <yumaeda@gmail.com>
2025-02-07 00:48:55 +0900

    altelier-de-lumiere.com: Configure website section for GCS

--- atelier-de-lumiere.com/google_storage_bucket.tf
+++ atelier-de-lumiere.com/google_storage_bucket.tf
@@ -3,6 +3,11 @@ resource "google_storage_bucket" "atelier_de_lumiere_com" {
   location      = var.region
   storage_class = "REGIONAL"
 
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+
   lifecycle_rule {
     condition {
       age = 30
